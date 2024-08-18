947515cce8f7be8d4858484de7d5765b10bcbe2a
Ranudi Gayathmie Kariyapperuma <ranudigk@gmail.com>
2024-08-18 19:25:14 +0530

    some coding parts

--- app/src/main/java/com/example/snakify/MainActivity.java
+++ app/src/main/java/com/example/snakify/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.snakify;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
+
+
     }
 }
--- app/src/main/java/com/example/snakify/signup.java
+++ app/src/main/java/com/example/snakify/signup.java
@@ -3,12 +3,18 @@ package com.example.snakify;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 
 public class signup extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_signup);
     }
 }
