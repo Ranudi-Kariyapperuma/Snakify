498e0f3f2ab7fa0a7e4c8218f311470070aae3a6
Ranudi Gayathmie Kariyapperuma <ranudigk@gmail.com>
2024-08-18 19:27:49 +0530

    make the front page codings

--- app/src/main/java/com/example/snakify/MainActivity.java
+++ app/src/main/java/com/example/snakify/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.snakify;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,7 +19,24 @@ public class MainActivity extends AppCompatActivity {
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_main);
+        Button button=findViewById(R.id.loginbtn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+                Intent intent=new Intent(MainActivity.this,login.class);
+                startActivity(intent);
+            }
+        });
 
+        Button btn=findViewById(R.id.signupbtn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent=new Intent(MainActivity.this,signup.class);
+                startActivity(intent);
+            }
+        });
     }
 }
